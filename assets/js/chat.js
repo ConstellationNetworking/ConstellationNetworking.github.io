c2d3cc236926c6a2c9d1ff236eb0d4fa7df0afbd
Jerry Hu <work.jerrywu@gmail.com>
2023-11-16 14:20:07 -0800

    updated code

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,14 +1,97 @@
 document.addEventListener('DOMContentLoaded', function () {
-    firebase.auth().onAuthStateChanged(function (user) {
+    const db = firebase.firestore();
+    const messageHistory = document.getElementById('message-history');
+    const sendButton = document.getElementById('send-button');
+    const messageInput = document.getElementById('message-input');
+    const otherUserID = document.getElementById('other-user-id').textContent; // Replace with actual ID
+
+    const auth = firebase.auth();
+
+    const updateChatHistory = (change) => {
+        const message = change.doc.data();
+        const messageElement = document.createElement('div');
+        messageElement.classList.add('message');
+        if (message.senderId === auth.currentUser.uid) {
+            messageElement.classList.add('sent');
+        } else {
+            messageElement.classList.add('received');
+        }
+        messageElement.textContent = message.messageText;
+        messageHistory.appendChild(messageElement);
+        messageHistory.scrollTop = messageHistory.scrollHeight;
+    };
+
+    auth.onAuthStateChanged(function (user) {
         if (user) {
             console.log('User signed in:', user);
+
+            const messagesCollection = db.collection('Messages');
+
+            // Listen for sent messages
+            messagesCollection
+                .where('senderId', '==', user.uid)
+                .where('receiverId', '==', otherUserID)
+                .orderBy('timestamp', 'asc')
+                .onSnapshot(snapshot => {
+                    snapshot.docChanges().forEach(change => {
+                        if (change.type === "added") {
+                            updateChatHistory(change);
+                        }
+                    });
+                });
+
+            // Listen for received messages
+            messagesCollection
+                .where('senderId', '==', otherUserID)
+                .where('receiverId', '==', user.uid)
+                .orderBy('timestamp', 'asc')
+                .onSnapshot(snapshot => {
+                    snapshot.docChanges().forEach(change => {
+                        if (change.type === "added") {
+                            updateChatHistory(change);
+                        }
+                    });
+                });
+
+            const sendMessage = () => {
+                const messageText = messageInput.value.trim();
+                if (messageText) {
+                    messagesCollection.add({
+                        senderId: user.uid,
+                        receiverId: otherUserID,
+                        messageText: messageText,
+                        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+                    })
+                    .then(() => {
+                        messageInput.value = '';
+                        console.log(`Message "${messageText}" sent successfully`);
+                    })
+                    .catch(error => {
+                        console.error("Error sending message: ", error);
+                        alert(`Error sending message: ${error}}`)
+                    });
+                }
+            };
+
+            sendButton.addEventListener('click', sendMessage);
+
+            messageInput.addEventListener('keydown', (e) => {
+                if (e.key === 'Enter' && !e.shiftKey) {
+                    e.preventDefault();
+                    sendMessage();
+                }
+            });
+
         } else {
             console.log('User is not signed in');
+            alert('Please sign in first. You will be directed to a sign in page.');
+            window.location.href = '/signin.html';
         }
     });
 
+    // Define the signout function here if needed
     function signout() {
-        firebase.auth().signOut()
+        auth.signOut()
             .then(() => {
                 console.log('User signed out successfully');
                 // Redirect or perform additional actions after sign-out
@@ -17,75 +100,5 @@ document.addEventListener('DOMContentLoaded', function () {
                 console.log('Sign-out error:', error);
                 // Handle sign-out error if needed
             });
-    }
-});
-
-const db = firebase.firestore();
-
-// Authentication state observer and get user data
-firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-        // User is signed in.
-        const currentUserID = user.uid; // The logged-in user's ID
-        const otherUserID = document.getElementById('other-user-id').textContent; // The other person in the conversation
-        const messagesCollection = db.collection('Messages');
-
-        // Query for messages in the direct conversation
-        const messagesQuery = messagesCollection
-            .where('senderId', 'in', [currentUserID, otherUserID])
-            .where('receiverId', 'in', [currentUserID, otherUserID])
-            .orderBy('timestamp', 'asc'); // Ordering by timestamp for chronological order
-
-        // Real-time listener for messages
-        messagesQuery.onSnapshot(snapshot => {
-            const messageHistory = document.getElementById('message-history');
-            // Clear existing messages in the UI
-            messageHistory.innerHTML = '';
-
-            snapshot.forEach(doc => {
-                const message = doc.data();
-
-                // Create message element
-                const messageElement = document.createElement('div');
-                messageElement.classList.add('message');
-                if (message.senderId === currentUserID) {
-                    messageElement.classList.add('sent');
-                } else {
-                    messageElement.classList.add('received');
-                }
-                messageElement.textContent = message.messageText;
-
-                // Append to message history
-                messageHistory.appendChild(messageElement);
-            });
-
-            // Scroll to the bottom of the message history
-            messageHistory.scrollTop = messageHistory.scrollHeight;
-        });
-
-        // Send message logic
-        document.getElementById('send-button').addEventListener('click', () => {
-            const messageInput = document.getElementById('message-input');
-            const messageText = messageInput.value.trim();
-
-            if (messageText) {
-                // Add a new message to the Messages collection
-                messagesCollection.add({
-                    senderId: currentUserID,
-                    receiverId: otherUserID,
-                    messageText: messageText,
-                    timestamp: firebase.firestore.FieldValue.serverTimestamp() // Sets the timestamp to the server time
-                })
-                    .then(() => {
-                        messageInput.value = ''; // Clear the message input field
-                    })
-                    .catch(error => {
-                        console.error("Error sending message: ", error);
-                    });
-            }
-        });
-    } else {
-        // User is signed out.
-        // ...
     }
 });
