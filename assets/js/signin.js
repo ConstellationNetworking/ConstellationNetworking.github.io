83495dfb064060c66172e7cc0972e412d94917a6
Jerry Hu <work.jerrywu@gmail.com>
2024-01-09 21:38:39 -0800

    Refactor new account initialization and signup process

--- assets/js/signin.js
+++ assets/js/signin.js
@@ -1,5 +1,3 @@
-import { newAccountInit } from "./helpers/new_account_init.js";
-
 var url = new URL(window.location.href);
 var searchParams = new URLSearchParams(url.search);
 var redirectURL = searchParams.get('redirect');
@@ -106,7 +104,63 @@ function signup() {
                                     alert('An verification link has been sent to your email. Please check your inbox to verify your account.');
                                     console.log(`Verification email sent to ${emailForm}`)
 
-                                    newAccountInit(user);
+                                    // tasks init
+                                    db.collection('Users').doc(user.uid).collection('Tasks').add({
+                                        task: "Create a new task by typing it above.",
+                                        completed: false,
+                                        createdAt: firebase.firestore.FieldValue.serverTimestamp()
+                                    })
+                                        .then(() => { })
+                                        .catch((error) => {
+                                            console.error(error);
+                                        })
+
+                                    // missions init
+                                    let missionID = generateUniqueId();
+                                    let missionRef = db.collection('Users').doc(user.uid).collection('Missions').doc(missionID);
+                                    let cardClasses = ['blue', 'green', 'yellow']
+
+                                    missionRef.set({
+                                        title: 'Welcome to your first mission!',
+                                        description: 'Creating your own unique presence through your person is the first step to building your network.',
+                                        completed: false,
+                                        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+                                        members: [user.uid],
+                                        tasks: { 'Edit your avatar.': false, 'Visit your missions page to see your missions.': false },
+                                        progress: 0,
+                                        type: 'Get started',
+                                        missionID: missionID,
+                                        cardColour: cardClasses[Math.floor(Math.random() * cardClasses.length)]
+                                    })
+                                        .then(() => { })
+                                        .catch((error) => {
+                                            console.error(error);
+                                        })
+
+                                    let missionID2 = generateUniqueId();
+                                    let missionRef2 = db.collection('Users').doc(user.uid).collection('Missions').doc(missionID2);
+
+                                    setTimeout(() => {
+                                        missionRef2.set({
+                                            title: 'Creating a todo',
+                                            description: 'Head over to your account and create a todo.',
+                                            completed: false,
+                                            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+                                            members: [user.uid],
+                                            tasks: { 'Visit your account page to see your tasks.': false },
+                                            progress: 0,
+                                            type: 'Get started',
+                                            missionID: missionID2,
+                                            cardColour: cardClasses[Math.floor(Math.random() * cardClasses.length)]
+                                        })
+                                            .then(() => {
+                                                // if (redirectURL != null) { window.location = redirectURL; } else { window.location = '/index.html'; }
+                                                window.location = '/setup_profile.html'
+                                            })
+                                            .catch((error) => {
+                                                console.error(error);
+                                            })
+                                    }, 1500);
                                 })
                         })
                         .catch((error) => {
@@ -146,3 +200,11 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 })
+
+function generateUniqueId() {
+    let id = Date.now().toString(36);
+    for(let i = 0; i < 5; i++) {
+        id += Math.random().toString(36).substr(2, 9);
+    }
+    return id;
+}
