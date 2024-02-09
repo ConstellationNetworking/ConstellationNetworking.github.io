8d103b06868ad99b7fbc3cc5c68e167622bfeb26
Jerry Hu <work.jerrywu@gmail.com>
2024-02-09 11:08:12 -0800

    added tailwindcss colours to dist

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,40 @@ module.exports = {
     './*/*.{html,js, css}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          100: '#f7fafc',
+        },
+        red: {
+          100: '#fff5f5',
+        },
+        orange: {
+          100: '#fffaf0',
+        },
+        yellow: {
+          100: '#fffff0',
+        },
+        green: {
+          100: '#f0fff4',
+        },
+        teal: {
+          100: '#e6fffa',
+        },
+        blue: {
+          100: '#ebf8ff',
+        },
+        indigo: {
+          100: '#ebf4ff',
+        },
+        purple: {
+          100: '#faf5ff',
+        },
+        pink: {
+          100: '#fff5f7',
+        },
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
