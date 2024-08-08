626d05fef431aa0b4229b87b269c15c21f5dbf6b
Jerry Hu <work.jerrywu@gmail.com>
2024-08-08 12:21:31 -0700

    updated account page design and added connections page

---
+++ assets/js/connections.js
@@ -0,0 +1,36 @@
+let db = firebase.firestore()
+let auth = firebase.auth();
+
+function smoothScrollAboveElement(elementId, offset) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        const elementRect = element.getBoundingClientRect();
+        const elementTop = elementRect.top + window.pageYOffset;
+        window.scrollTo({
+            top: elementTop - offset,
+            behavior: 'smooth'
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    auth.onAuthStateChanged(function(user) {
+        if (user) {
+            const userRef = db.collection("Users").doc(auth.currentUser.uid);
+            const lastActive = new Date(); // Current timestamp
+            userRef.set({
+                lastActive: lastActive
+            }, { merge: true });
+            
+            userRef.get().then((doc) => {
+                if (doc.exists) {
+                    const data = doc.data();
+                    
+                    document.getElementById('user-profile-img').src = data.profileIMG == "" ? '/assets/img/default_user.jpeg' : data.profileIMG
+                }
+            })
+        } else {
+            window.location.href = '/signin.html';
+        }
+    })
+});
