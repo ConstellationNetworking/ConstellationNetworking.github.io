8ef9472322d768afc0ce46ac618612d478e8b275
AccessRetrieved <56012931+AccessRetrieved@users.noreply.github.com>
2025-01-10 06:15:25 +0000

    Deploying to gh-pages from @ ConstellationNetworking/ConstellationNetworking.github.io@7605ed4d6c3b56c3a3f9fcd18bc0fed58dddd736 🚀

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,82 +1,99 @@
-let db = firebase.firestore();
+document.addEventListener('DOMContentLoaded', function () {
+    try {
+        let db = firebase.firestore();
+        let auth = firebase.auth();
+    } catch {
+        console.error('[ERROR] Unable to load firebase db & auth.')
+    }
 
-function smoothScrollAboveElement(elementId, offset) {
-    const element = document.getElementById(elementId);
-    if (element) {
-        const elementRect = element.getBoundingClientRect();
-        const elementTop = elementRect.top + window.pageYOffset;
-        window.scrollTo({
-            top: elementTop - offset,
-            behavior: 'smooth'
+    function switchBg(bg) {
+        var images = {
+            'bg1.jpg': 'Joshua Earle',
+            'bg2.jpg': 'Martin Jernberg',
+            'bg3.jpg': 'Juskteez Vu',
+            't1.png': 'Jason Leung',
+            't2.png': 'Pawel Czerwinski'
+        };
+
+        if (bg) {
+            var randomImage = bg;
+            var photographer = images[bg];
+        } else {
+            var keys = Object.keys(images);
+            var randomImage = keys[Math.floor(Math.random() * keys.length)];
+            var photographer = images[randomImage];
+
+            auth.onAuthStateChanged(function (user) {
+                if (user) {
+                    const userRef = db.collection('Users').doc(user.uid)
+                    userRef.update({
+                        bg: randomImage
+                    }).then(() => {
+                        console.log('Updated bg image.');
+                    }).catch((error) => {
+                        console.error(error);
+                    })
+                }
+            })
+        }
+
+        var body = document.getElementById('body');
+        body.style.backgroundImage = 'url(/assets/img/index/' + randomImage + ')';
+
+        var citation = document.getElementById('photo-citation')
+        citation.innerHTML = 'Photo by ' + photographer + ' on <a href="https://unsplash.com" style="text-decoration: underline;">Unsplash</a>';
+        citation.style.cursor = 'pointer';
+
+        var aTag = citation.querySelector('a');
+        aTag.style.color = 'gray';
+        aTag.style.transition = 'color 0.3s';
+        aTag.addEventListener('mouseover', function () {
+            aTag.style.color = 'white';
+        });
+        aTag.addEventListener('mouseout', function () {
+            aTag.style.color = 'gray';
         });
     }
-}
 
-function submitForm() {
-    email = document.getElementById('email').value;
+    try {
+        firebase.auth().onAuthStateChanged(function (user) {
+            const accountManagementLink = document.getElementById('accountMgnLink');
+            const accountManagementText = document.getElementById('accountMgnText');
+            const join_link = document.getElementById('join-link');
 
-    if (!email) {
-        document.getElementById('email').placeholder = 'Please enter a valid email address';
-        return;
-    }
+            if (user) {
+                const userRef = db.collection('Users').doc(user.uid);
+                const lastActive = firebase.firestore.FieldValue.serverTimestamp()
+                userRef.set({
+                    lastActive: lastActive
+                }, { merge: true });
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-        document.getElementById('email').value = '';
-        document.getElementById('email').placeholder = 'Please enter a valid email address';
-        return;
-    }
+                userRef.get().then((doc) => {
+                    if (doc.exists) {
+                        const data = doc.data();
+                        switchBg(data.bg)
+                    } else {
+                        console.log("No such user.");
+                    }
+                }).catch((error) => {
+                    console.log("Error getting document:", error);
+                });
 
-    db.collection('ComingSoonSubscribers').add({
-        email: email,
-        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-        browserInfo: navigator.userAgent
-    })
-    .then((docRef) => {
-        document.getElementById('email').value = '';
-        document.getElementById('email').placeholder = 'Thank you for subscribing!';
-        document.getElementById('submitBtn').innerHTML = 'Done!';
-    })
-}
+                accountManagementLink.onclick = function () { firebase.auth().signOut() }
+                accountManagementText.innerText = 'Log Out';
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    const db = firebase.firestore();
-    const tableBody = document.getElementById('positions-table-body');
+                join_link.innerHTML = 'Chat Now';
+                join_link.onclick = function () { window.location = '/chat.html' };
+            } else {
+                switchBg('bg3.jpg')
+                accountManagementLink.onclick = function () { window.location = '/signin.html?redirect=/index.html' }
+                accountManagementText.innerText = 'Log in';
 
-    db.collection('Open_Volunteer').get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            const data = doc.data();
-
-            const row = document.createElement('tr');
-
-            const titleCell = document.createElement('td');
-            titleCell.className = 'pr-6 py-4 whitespace-nowrap'
-            titleCell.textContent = data.title;
-            row.appendChild(titleCell);
-
-            const aboutCell = document.createElement('td');
-            aboutCell.textContent = data.about;
-            row.appendChild(aboutCell);
-
-            const hoursCell = document.createElement('td');
-            hoursCell.textContent = data.hours;
-            row.appendChild(hoursCell);
-
-            const locationCell = document.createElement('td');
-            locationCell.textContent = data.location;
-            row.appendChild(locationCell);
-
-            const actionCell = document.createElement('td');
-            const actionLink = document.createElement('a');
-            actionLink.href = data.url;
-            const actionSpan = document.createElement('span');
-            actionSpan.textContent = data.action;
-            actionSpan.className = `px-3 py-2 rounded-full font-semibold text-xs ${data.action === 'Active' ? 'text-green-600 bg-green-50' : 'text-blue-600 bg-blue-50'}`;
-            actionLink.appendChild(actionSpan);
-            actionCell.appendChild(actionLink);
-            row.appendChild(actionCell);
-
-            tableBody.appendChild(row);
+                join_link.innerHTML = 'View our missions';
+                join_link.onclick = function () { window.location = '/about.html' };
+            }
         });
-    });
+    } catch {
+        switchBg('bg3.jpg')
+    }
 });
