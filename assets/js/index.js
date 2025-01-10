75a6b0aaae94e043f4ebe9699419d2622434aaff
AccessRetrieved <56012931+AccessRetrieved@users.noreply.github.com>
2025-01-10 07:10:39 +0000

    Deploying to gh-pages from @ ConstellationNetworking/ConstellationNetworking.github.io@7a9e336e8838cdbc315070cfe69c8dc04d779a99 🚀

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,5 @@
-try {
-    let db = firebase.firestore();
-    let auth = firebase.auth();
-} catch {
-    console.error('[ERROR] Unable to load firebase db & auth.')
-}
+let db = firebase.firestore();
+let auth = firebase.auth();
 
 function switchBg(bg) {
     var images = {
@@ -55,45 +51,44 @@ function switchBg(bg) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    try {
-        firebase.auth().onAuthStateChanged(function (user) {
-            const accountManagementLink = document.getElementById('accountMgnLink');
-            const accountManagementText = document.getElementById('accountMgnText');
-            const join_link = document.getElementById('join-link');
-    
-            if (user) {
-                const userRef = db.collection('Users').doc(user.uid);
-                const lastActive = firebase.firestore.FieldValue.serverTimestamp()
-                userRef.set({
-                    lastActive: lastActive
-                }, { merge: true });
-    
-                userRef.get().then((doc) => {
-                    if (doc.exists) {
-                        const data = doc.data();
-                        switchBg(data.bg)
-                    } else {
-                        console.log("No such user.");
-                    }
-                }).catch((error) => {
-                    console.log("Error getting document:", error);
-                });
-    
-                accountManagementLink.onclick = function () { firebase.auth().signOut() }
-                accountManagementText.innerText = 'Log Out';
-    
-                join_link.innerHTML = 'Chat Now';
-                join_link.onclick = function () { window.location = '/chat.html' };
-            } else {
-                switchBg('bg3.jpg')
-                accountManagementLink.onclick = function () { window.location = '/signin.html?redirect=/index.html' }
-                accountManagementText.innerText = 'Log in';
-    
-                join_link.innerHTML = 'View our missions';
-                join_link.onclick = function () { window.location = '/about.html' };
-            }
-        });
-    } catch {
-        switchBg('bg3.jpg')
-    }
+    const db = firebase.firestore();
+    const auth = firebase.auth();
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        const accountManagementLink = document.getElementById('accountMgnLink');
+        const accountManagementText = document.getElementById('accountMgnText');
+        const join_link = document.getElementById('join-link');
+
+        if (user) {
+            const userRef = db.collection('Users').doc(user.uid);
+            const lastActive = firebase.firestore.FieldValue.serverTimestamp()
+            userRef.set({
+                lastActive: lastActive
+            }, { merge: true });
+
+            userRef.get().then((doc) => {
+                if (doc.exists) {
+                    const data = doc.data();
+                    switchBg(data.bg)
+                } else {
+                    console.log("No such user.");
+                }
+            }).catch((error) => {
+                console.log("Error getting document:", error);
+            });
+
+            accountManagementLink.onclick = function () { firebase.auth().signOut() }
+            accountManagementText.innerText = 'Log Out';
+
+            join_link.innerHTML = 'Chat Now';
+            join_link.onclick = function () { window.location = '/chat.html' };
+        } else {
+            switchBg('bg3.jpg')
+            accountManagementLink.onclick = function () { window.location = '/signin.html?redirect=/index.html' }
+            accountManagementText.innerText = 'Log in';
+
+            join_link.innerHTML = 'View our missions';
+            join_link.onclick = function () { window.location = '/about.html' };
+        }
+    });
 });
