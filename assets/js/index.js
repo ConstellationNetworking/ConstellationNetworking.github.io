5ce8ca165aa73e7eb71dbc411e940ac7dfd64fe1
AccessRetrieved <56012931+AccessRetrieved@users.noreply.github.com>
2025-01-10 06:55:48 +0000

    Deploying to gh-pages from @ ConstellationNetworking/ConstellationNetworking.github.io@6cc4c522792d614d6ec6e4224d5f63da47cb4741 🚀

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,9 @@
-let db = firebase.firestore();
-let auth = firebase.auth();
+try {
+    let db = firebase.firestore();
+    let auth = firebase.auth();
+} catch {
+    console.error('[ERROR] Unable to load firebase db & auth.')
+}
 
 function switchBg(bg) {
     var images = {
@@ -51,42 +55,45 @@ function switchBg(bg) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    firebase.auth().onAuthStateChanged(function (user) {
-        const accountManagementLink = document.getElementById('accountMgnLink');
-        const accountManagementText = document.getElementById('accountMgnText');
-        const join_link = document.getElementById('join-link');
-
-        if (user) {
-            let db = firebase.firestore();
-            const userRef = db.collection('Users').doc(user.uid);
-            const lastActive = firebase.firestore.FieldValue.serverTimestamp()
-            userRef.set({
-                lastActive: lastActive
-            }, { merge: true });
-
-            userRef.get().then((doc) => {
-                if (doc.exists) {
-                    const data = doc.data();
-                    switchBg(data.bg)
-                } else {
-                    console.log("No such user.");
-                }
-            }).catch((error) => {
-                console.log("Error getting document:", error);
-            });
-
-            accountManagementLink.onclick = function () { firebase.auth().signOut() }
-            accountManagementText.innerText = 'Log Out';
-
-            join_link.innerHTML = 'Chat Now';
-            join_link.onclick = function () { window.location = '/chat.html' };
-        } else {
-            switchBg('bg3.jpg')
-            accountManagementLink.onclick = function () { window.location = '/signin.html?redirect=/index.html' }
-            accountManagementText.innerText = 'Log in';
-
-            join_link.innerHTML = 'View our missions';
-            join_link.onclick = function () { window.location = '/about.html' };
-        }
-    });
+    try {
+        firebase.auth().onAuthStateChanged(function (user) {
+            const accountManagementLink = document.getElementById('accountMgnLink');
+            const accountManagementText = document.getElementById('accountMgnText');
+            const join_link = document.getElementById('join-link');
+    
+            if (user) {
+                const userRef = db.collection('Users').doc(user.uid);
+                const lastActive = firebase.firestore.FieldValue.serverTimestamp()
+                userRef.set({
+                    lastActive: lastActive
+                }, { merge: true });
+    
+                userRef.get().then((doc) => {
+                    if (doc.exists) {
+                        const data = doc.data();
+                        switchBg(data.bg)
+                    } else {
+                        console.log("No such user.");
+                    }
+                }).catch((error) => {
+                    console.log("Error getting document:", error);
+                });
+    
+                accountManagementLink.onclick = function () { firebase.auth().signOut() }
+                accountManagementText.innerText = 'Log Out';
+    
+                join_link.innerHTML = 'Chat Now';
+                join_link.onclick = function () { window.location = '/chat.html' };
+            } else {
+                switchBg('bg3.jpg')
+                accountManagementLink.onclick = function () { window.location = '/signin.html?redirect=/index.html' }
+                accountManagementText.innerText = 'Log in';
+    
+                join_link.innerHTML = 'View our missions';
+                join_link.onclick = function () { window.location = '/about.html' };
+            }
+        });
+    } catch {
+        switchBg('bg3.jpg')
+    }
 });
--- index.js
+++
@@ -1,6 +0,0 @@
-try {
-    let db = firebase.firestore();
-    let auth = firebase.auth();
-} catch {
-    console.error('[ERROR] Unable to load firebase db & auth.');
-} 
